5a3f7e55771df4615d6abe0e1cd5430a7a548a47
malnick <malnick@gmail..com>
2016-05-17 11:55:07 -0700

    yaml file

---
+++ action/generate.go
@@ -0,0 +1,42 @@
+package action
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/grafana/grafana/pkg/log"
+	"github.com/malnick/cryptorious/config"
+)
+
+// GenerateKeys() creates public private keys for a $USER
+func GenerateKeys(c config.Config) {
+	keyPath := c.KeyDirectory
+	username := c.Username
+	privPath := fmt.Sprintf("%s/privatekey_%s.pem", keyPath, username)
+	pubPath := fmt.Sprintf("%s/publickey_%s.pem", keyPath, username)
+	// generate private key
+	privatekey, err := rsa.GenerateKey(rand.Reader, 1024)
+	checkError(err)
+	// Write Private Key
+	privBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privatekey),
+	})
+	ioutil.WriteFile(privPath, privBytes, 0600)
+	log.Info("Private Key: ", privPath)
+	fmt.Println(string(privBytes))
+	// Write Public Key
+	ansipub, err := x509.MarshalPKIXPublicKey(&privatekey.PublicKey)
+	checkError(err)
+	pubBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: ansipub,
+	})
+	ioutil.WriteFile(pubPath, pubBytes, 0644)
+	log.Info("Public Key: ", pubPath)
+	fmt.Println(string(pubBytes))
+}
--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
 var (
 	VERSION  string
 	REVISION string
@@ -7,14 +13,47 @@ var (
 
 // Config{} is the complete application configuration
 type Config struct {
-	Version  string
-	Revision string
+	Version        string `yaml:",omitempty"`
+	Revision       string `yaml:",omitempty"`
+	AppYamlPath    string `yaml:",omitempty"`
+	PrivateKeyPath string `yaml:"private_key_path"`
+	PublicKeyPath  string `yaml:"public_key_path"`
+	VaultPath      string `yaml:"vault_path"`
+	UserName       string `yaml:"user_name"`
 }
 
-//
-func Configuration() (c Config, err error) {
-	return c{
-		Version:  VERSION,
-		Revision: REVISION,
-	}, nil
+func (c *Config) Set() error {
+	if len(c.AppYamlPath) > 0 {
+
+	} else {
+		c.Version = VERSION
+		c.Revision = REVISION
+		c.UserName = ""
+		c.PrivateKeyPath = "./cryptorious_privatekey"
+		c.PublicKeyPath = "./cryptorious_publickey"
+		c.VaultPath = "./cryptorious_vault.yaml"
+		c.AppYamlPath = "./cryptorious.yaml"
+
+		yamlBytes, err := yaml.Marshal(&c)
+		if err != nil {
+			return err
+		}
+		if err := ioutil.WriteFile(c.AppYamlPath, yamlBytes, 0644); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Config) Load() error {
+
+	return nil
+}
+
+// Configuration() returns the configuration for application level logic
+func GetConfiguration() (c Config, err error) {
+	if err := c.Set(); err != nil {
+		return c, err
+	}
+	return c, nil
 }
